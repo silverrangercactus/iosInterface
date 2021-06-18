987dd03288b18efe0878e8c4867cba9a97342995
silverrangercactus <silverrangercactus@gmail.com>
2021-06-18 10:23:13 +0500

    Create project

---
+++ homework-1.2/homework-1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework-1.2
+//
+//  Created by Fedor Boriskin on 18.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
